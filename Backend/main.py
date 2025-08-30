77b8068bd3c73b2a93a5caabaf5287050820b0c8
purab <abc783848@gmail.com>
2025-08-31 01:00:47 +0530

    latitude/longitude

--- Backend/main.py
+++ Backend/main.py
@@ -47,7 +47,7 @@ LANE_VIDEO_MAP = {
     'west': '4.mp4',
 }
 
-TRAFFIC_LIGHT_COORDS = (28.7198611, 77.2621111)
+TRAFFIC_LIGHT_COORDS = ()
 AMBULANCE_OVERRIDE_DURATION = 30  # seconds
 ambulance_override = {
     'active': False,
